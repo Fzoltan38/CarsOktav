3f3b876b093e828d8aaad0ec2d420c23ec3b3ad3
FZoltan40 <farkasz@kkszki.hu>
2025-11-22 08:08:42 +0100

    Post enpoint is ready.

---
+++ CarsApi/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsApi
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ CarsApi/Controllers/CarsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace CarsApi.Controllers
+{
+    [Route("cars")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public const string ConnectionString = "Server=localhost;Database=trader;Uid=root; Password=;SslMode=None";
+        [HttpPost]
+        public object AddNewCar(Car car)
+        {
+            try
+            {
+                string sql = "INSERT INTO `cars`(`Brand`, `Type`, `Color`, `Year`) VALUES (@brand,@type,@color,@year)";
+
+                using (var connect = new MySqlConnection(ConnectionString))
+                {
+                    connect.Open();
+                    MySqlCommand cmd = new MySqlCommand(sql, connect);
+
+                    cmd.Parameters.AddWithValue("@brand", car.Brand);
+                    cmd.Parameters.AddWithValue("@type", car.Type);
+                    cmd.Parameters.AddWithValue("@color", car.Color);
+                    cmd.Parameters.AddWithValue("@year", car.Year);
+
+                    cmd.ExecuteNonQuery();
+                    connect.Close();
+                }
+
+                return new { message = "Sikres felvétel.", result = car };
+            }
+            catch (Exception ex)
+            {
+                return new { message = "Sikretelen felvétel.", result = ex.Message };
+            }
+          
+        }
+    }
+}
